ef1eb11ab056147bbf4adc2ac599da9108a917ff
sivahari01 <115487259+sivahari01@users.noreply.github.com>
2025-03-02 02:01:36 +0530

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "s3buck" {
   bucket = "myvtfitestsbucket1232"
-  acl    = "public" 
+  acl = "public-read"
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
