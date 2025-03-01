50fc15fa4c58b294e889a0fe50dfd381b982579f
sivahari01 <115487259+sivahari01@users.noreply.github.com>
2025-03-02 02:32:48 +0530

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,7 +1,13 @@
 resource "aws_s3_bucket" "s3buck" {
   bucket = "myvtfitestsbucket1232"
+  
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
+  }
+
+  public_access_block_configuration {
+    block_public_acls   = false
+    block_public_policy = false
   }
 }
