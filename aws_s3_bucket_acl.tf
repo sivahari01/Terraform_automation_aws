5ca9bdb8e0e0d060d6e9c6ecc60b45ac1682e54a
sivahari01 <115487259+sivahari01@users.noreply.github.com>
2025-03-02 02:30:15 +0530

    Update aws_s3_bucket_acl.tf

--- aws_s3_bucket_acl.tf
+++ aws_s3_bucket_acl.tf
@@ -1,4 +1,14 @@
-resource "aws_s3_bucket_acl" "s3buck_acl" {
+resource "aws_s3_bucket_policy" "s3buck_policy" {
   bucket = aws_s3_bucket.s3buck.id
-  acl    = "public-read"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.s3buck.arn}/*"
+      }
+    ]
+  })
 }
