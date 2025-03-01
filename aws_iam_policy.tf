ec4b8853368c5b58762a17f7ad41ab7e749a8b71
sivahari01 <115487259+sivahari01@users.noreply.github.com>
2025-03-02 04:17:40 +0530

    Update aws_iam_policy.tf

--- aws_iam_policy.tf
+++ aws_iam_policy.tf
@@ -1,17 +1,14 @@
 resource "aws_iam_policy" "s3_policy" {
-  name        = "s3_full_access_policy"
-  description = "Policy to allow EC2 instance to access S3"
-  
-  policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
+  name        = "S3FullAccessPolicy"
+  description = "Policy to grant full access to S3"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
       {
-        "Effect": "Allow",
-        "Action": "s3:*",
-        "Resource": "*"
-      }
+        Action   = "s3:*"
+        Resource = "arn:aws:s3:::*"
+        Effect   = "Allow"
+      },
     ]
-  }
-  EOF
+  })
 }
