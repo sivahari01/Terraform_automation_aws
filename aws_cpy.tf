04d8280d9d3b99a0553008608e7acd0cd3c86281
sivahari01 <115487259+sivahari01@users.noreply.github.com>
2025-03-02 04:57:50 +0530

    Create aws_cpy.tf

---
+++ aws_cpy.tf
@@ -0,0 +1,7 @@
+# Upload file to the S3 bucket
+resource "aws_s3_object" "html_zip" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+  key    = "html.zip"   # The name of the file in S3
+  source = "/root/html.zip"   # Path to your local file
+  acl    = "public-read"  # Set ACL if needed (e.g., public-read)
+}
