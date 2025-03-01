c77e80936dc859c4b669e9775d30ea2bdf628462
sivahari01 <115487259+sivahari01@users.noreply.github.com>
2025-03-02 04:17:21 +0530

    Update aws_iam_role.tf

--- aws_iam_role.tf
+++ aws_iam_role.tf
@@ -1,15 +1,17 @@
 resource "aws_iam_role" "s3_role" {
-  name               = "s3-access-role"
+  name = "EC2-S3-Role"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Action    = "sts:AssumeRole"
-        Effect    = "Allow"
         Principal = {
           Service = "ec2.amazonaws.com"
         }
-      }
+        Effect    = "Allow"
+        Sid       = ""
+      },
     ]
   })
 }
