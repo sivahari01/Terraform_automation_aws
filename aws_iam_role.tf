88654cc3ec1f9ab0ea8a5cd30344237022cc1da6
sivahari01 <115487259+sivahari01@users.noreply.github.com>
2025-03-02 04:09:16 +0530

    Update aws_iam_role.tf

--- aws_iam_role.tf
+++ aws_iam_role.tf
@@ -1,18 +1,15 @@
-resource "aws_iam_role" "s3_access_role" {
-  name = "s3-access-role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+resource "aws_iam_role" "s3_role" {
+  name               = "s3-access-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
