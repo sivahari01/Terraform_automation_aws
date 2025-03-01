ee107cf74b5ee734c9fc2c5f2ea01096bf4b0ac3
sivahari01 <115487259+sivahari01@users.noreply.github.com>
2025-03-02 04:24:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,4 +8,6 @@ terraform {
 }
 provider "aws" {
   region = "us-east-1"  # Specify the AWS region
+  access_key = "your-access-key-id"       # Your AWS Access Key ID
+  secret_key = "your-secret-access-key"   # Your AWS Secret Access Ke
 }
