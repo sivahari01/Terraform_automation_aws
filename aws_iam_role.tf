195f93c160612159c501c14709179edb51bfb74d
sivahari01 <115487259+sivahari01@users.noreply.github.com>
2025-03-02 03:47:09 +0530

    Create aws_iam_role.tf

---
+++ aws_iam_role.tf
@@ -0,0 +1,18 @@
+resource "aws_iam_role" "s3_access_role" {
+  name = "s3-access-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
