74764473f9fa968ab7dd0d03fa6803d850c4593c
sivahari01 <115487259+sivahari01@users.noreply.github.com>
2025-03-02 02:41:42 +0530

    Update aws_s3_bucket_acl.tf

--- aws_s3_bucket_acl.tf
+++ aws_s3_bucket_acl.tf
@@ -1,12 +1,12 @@
 resource "aws_s3_bucket_policy" "s3buck_policy" {
   bucket = aws_s3_bucket.s3buck.id
   policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow"
-        Principal = "*"
-        Action = "s3:GetObject"
+        Effect = "Allow",
+        Principal = "*", # ❌ Public access is blocked
+        Action = "s3:GetObject",
         Resource = "${aws_s3_bucket.s3buck.arn}/*"
       }
     ]
