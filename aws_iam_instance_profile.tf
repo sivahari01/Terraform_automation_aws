8a5fc97a0f65edfc920a167da9922385b31002bb
sivahari01 <115487259+sivahari01@users.noreply.github.com>
2025-03-02 04:18:29 +0530

    Update aws_iam_instance_profile.tf

--- aws_iam_instance_profile.tf
+++ aws_iam_instance_profile.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_instance_profile" "s3_profile" {
-  name = "s3-access-instance-profile"
+resource "aws_iam_instance_profile" "s3_instance_profile" {
+  name = "EC2-S3-Instance-Profile"
   role = aws_iam_role.s3_role.name
 }
