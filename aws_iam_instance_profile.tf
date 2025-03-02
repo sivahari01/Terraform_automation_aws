0a63fa8bc4d30e562348cacc516090fae37f2caf
sivahari01 <115487259+sivahari01@users.noreply.github.com>
2025-03-02 21:41:21 +0530

    Update aws_iam_instance_profile.tf

--- aws_iam_instance_profile.tf
+++ aws_iam_instance_profile.tf
@@ -1,15 +1,14 @@
-# Fetch existing IAM instance profile if it exists
+# Fetch existing IAM instance profile
 data "aws_iam_instance_profile" "existing_s3_profile" {
   name = "EC2-S3-Instance-Profile"
 }
 
-# Create only if it does not exist
+# Create IAM instance profile only if it does not exist
 resource "aws_iam_instance_profile" "s3_instance_profile" {
-  count = length(data.aws_iam_instance_profile.existing_s3_profile.id) > 0 ? 0 : 1
   name  = "EC2-S3-Instance-Profile"
   role  = aws_iam_role.s3_role.name
 
-lifecycle {
+  lifecycle {
     prevent_destroy = true
   }
 }
