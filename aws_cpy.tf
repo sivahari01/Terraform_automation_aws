4e7fc76bbe6755e88d468703e0602a0dbbb0d08e
sivahari01 <115487259+sivahari01@users.noreply.github.com>
2025-03-02 21:12:22 +0530

    Delete aws_cpy.tf

--- aws_cpy.tf
+++
@@ -1,7 +0,0 @@
-# Upload file to the S3 bucket
-resource "aws_s3_object" "html_zip" {
-  bucket = aws_s3_bucket.s3buck.bucket
-  key    = "html.zip"   # The name of the file in S3
-  source = "/root/html.zip"   # Path to your local file
-  acl    = "public-read"  # Set ACL if needed (e.g., public-read)
-}
