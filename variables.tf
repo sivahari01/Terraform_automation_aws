43bf3d2867a2c23f6f2b70dd1cf8d6302e2890e2
sivahari01 <115487259+sivahari01@users.noreply.github.com>
2025-03-09 21:45:47 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
