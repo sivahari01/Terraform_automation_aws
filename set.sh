28275cf7f786dee4df718113e5aa76f7dcab6fa1
sivahari01 <115487259+sivahari01@users.noreply.github.com>
2025-03-09 23:20:06 +0530

    Update set.sh

--- set.sh
+++ set.sh
@@ -46,10 +46,10 @@ sudo zip -r /root/html_report.zip /root/html_report
   sudo mkdir -p ~/.aws
   sudo touch  ~/.aws/credentials
   sleep 10s
-  echo "
-  [default]
+  echo "[default]
   aws_access_key_id=${var.AWS_ACCESS_KEY_ID}
-  aws_secret_access_key=${var.AWS_SECRET_ACCESS_KEY} " > ~/.aws/credentials
+  aws_secret_access_key=${var.AWS_SECRET_ACCESS_KEY} " | sudo tee /root/.aws/credentials > /dev/null
+
 
 sleep 5s
 # Set correct permissions
